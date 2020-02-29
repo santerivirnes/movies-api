95044af88e41bccaf53f3635f91bb77029a29b0c
santerivirnes <santeri.virnes@gmail.com>
2020-02-29 17:46:48 +0200

    returns more data

--- api.py
+++ api.py
@@ -4,25 +4,34 @@ from rake_nltk import Rake
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
+from flask_cors import CORS, cross_origin
 
 df = pd.read_csv('https://query.data.world/s/uikepcpffyo2nhig52xxeevdialfl7')
 
-df = df[['Title','Genre','Director','Actors','Plot']]
+df = df[['Title','Genre','Director','Actors','Plot', 'imdbID', 'Poster']]
+
+final_data = df[['Title', 'imdbID', 'Poster']]
 
 
 # discarding the commas between the actors' full names and getting only the first three names
 df['Actors'] = df['Actors'].map(lambda x: x.split(',')[:3])
 
+
+
 # putting the genres in a list of words
 df['Genre'] = df['Genre'].map(lambda x: x.lower().split(','))
 
+
 df['Director'] = df['Director'].map(lambda x: x.split(' '))
+
+
 
 # merging together first and last name for each actor and director, so it's considered as one word 
 # and there is no mix up between people sharing a first name
 for index, row in df.iterrows():
     row['Actors'] = [x.lower().replace(' ','') for x in row['Actors']]
     row['Director'] = ''.join(row['Director']).lower()
+
 
 
 # initializing the new column
@@ -46,8 +55,11 @@ for index, row in df.iterrows():
 
 # dropping the Plot column
 df.drop(columns = ['Plot'], inplace = True)
+# dropping the Plot column
+df.drop(columns = ['Title'], inplace = True)
+df.set_index('imdbID', inplace = True)
 
-df.set_index('Title', inplace = True)
+
 
 df['bag_of_words'] = ''
 columns = df.columns
@@ -59,18 +71,23 @@ for index, row in df.iterrows():
         else:
             words = words + row[col]+ ' '
     row['bag_of_words'] = words
-    
 df.drop(columns = [col for col in df.columns if col!= 'bag_of_words'], inplace = True)
+
+
 
 
 # instantiating and generating the count matrix
 count = CountVectorizer()
 count_matrix = count.fit_transform(df['bag_of_words'])
 
+
+
 # creating a Series for the movie titles so they are associated to an ordered numerical
 # list I will use later to match the indexes
 indices = pd.Series(df.index)
 indices[:5]
+
+
 
 # generating the cosine similarity matrix
 cosine_sim = cosine_similarity(count_matrix, count_matrix)
@@ -79,31 +96,40 @@ cosine_sim = cosine_similarity(count_matrix, count_matrix)
 def recommendations(title, cosine_sim = cosine_sim):
     
     recommended_movies = []
-    
-    # gettin the index of the movie that matches the title
-    idx = indices[indices == title].index[0]
 
-    # creating a Series with the similarity scores in descending order
-    score_series = pd.Series(cosine_sim[idx]).sort_values(ascending = False)
+    if title in indices.values:
+            
 
-    # getting the indexes of the 10 most similar movies
-    top_10_indexes = list(score_series.iloc[1:11].index)
-    
-    # populating the list with the titles of the best 10 matching movies
-    for i in top_10_indexes:
-        recommended_movies.append(list(df.index)[i])
+        # gettin the index of the movie that matches the title
+        idx = indices[indices == title].index[0]
         
-    return recommended_movies
+        # creating a Series with the similarity scores in descending order
+        score_series = pd.Series(cosine_sim[idx]).sort_values(ascending = False)
+
+        
+        
+        # getting the indexes of the 10 most similar movies
+        top_10_indexes = list(score_series.iloc[1:11].index)
+        
+
+        # populating the list with the titles of the best 10 matching movies
+        for i in top_10_indexes:
+            recommended_movies.append({'Title': list(final_data['Title'])[i], 'imdbID': list(final_data['imdbID'])[i], 'Poster': list(final_data['Poster'])[i]})
+    
+        return recommended_movies
+    else:
+        return recommended_movies
 
 
 
 api = Flask(__name__)
-
+CORS(api)
 
 @api.route('/movies', methods=['GET', 'POST'])
+
 def get_movies():
   data = request.get_json()
-  return json.dumps(recommendations(data['name']))
+  return json.dumps(recommendations(data['id']))
   
   
   
