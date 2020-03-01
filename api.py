b8ebf122cb56adf8c03115540e2be46326b99702
santerivirnes <santeri.virnes@gmail.com>
2020-03-01 22:01:52 +0200

    Kommenttien muokkausta

--- api.py
+++ api.py
@@ -6,61 +6,66 @@ from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 from flask_cors import CORS, cross_origin
 
+#luodaan taulukko csv tiedostosta
 df = pd.read_csv('https://query.data.world/s/uikepcpffyo2nhig52xxeevdialfl7')
 
-df = df[['Title','Genre','Director','Actors','Plot', 'imdbID', 'Poster']]
 
+#napataan taulukosta kyseiset kolumnit
+df = df[['Title','Genre','Director','Actors','Plot' ,'Poster', 'imdbID']]
+
+#tehdään toinen taulukko myöhempää käyttöä varten
 final_data = df[['Title', 'imdbID', 'Poster']]
 
 
-# discarding the commas between the actors' full names and getting only the first three names
+# poistetaan pilkku näyttelijöiden nimien välistä ja napataan vain 3 ensimmäistä nimeä talteen
 df['Actors'] = df['Actors'].map(lambda x: x.split(',')[:3])
 
 
 
-# putting the genres in a list of words
+# asetetaan genret listaan
 df['Genre'] = df['Genre'].map(lambda x: x.lower().split(','))
 
-
+# Poistetaan välit ohjaajien nimistä
 df['Director'] = df['Director'].map(lambda x: x.split(' '))
 
 
 
-# merging together first and last name for each actor and director, so it's considered as one word 
-# and there is no mix up between people sharing a first name
+# yhdistetään etu ja sukunimet, jotta saman etunimen kanssa ei tule sekaannuksia
 for index, row in df.iterrows():
     row['Actors'] = [x.lower().replace(' ','') for x in row['Actors']]
     row['Director'] = ''.join(row['Director']).lower()
 
 
 
-# initializing the new column
+# Uusi kolumni johon juonen avain sanat paloitellaan
 df['Key_words'] = ""
 
+
+#Puretaan juoni avain sanoiksi ja talletaan ne edellä luotuun kolumniin
 for index, row in df.iterrows():
     plot = row['Plot']
     
-    # instantiating Rake, by default is uses english stopwords from NLTK
-    # and discard all puntuation characters
+    # instantiating Rake, by default is uses english stopwords from NLTK, nämä kommentit säästetään tutorialista
+    # and discard all puntuation characters, nämä kommentit säästetään tutorialista
     r = Rake()
 
-    # extracting the words by passing the text
+    # extracting the words by passing the text, nämä kommentit säästetään tutorialista
     r.extract_keywords_from_text(plot)
 
-    # getting the dictionary whith key words and their scores
+    # getting the dictionary whith key words and their scores, nämä kommentit säästetään tutorialista
     key_words_dict_scores = r.get_word_degrees()
     
-    # assigning the key words to the new column
+    # assigning the key words to the new column, nämä kommentit säästetään tutorialista
     row['Key_words'] = list(key_words_dict_scores.keys())
 
-# dropping the Plot column
+# Poistetaan juoni kolumni
 df.drop(columns = ['Plot'], inplace = True)
-# dropping the Plot column
+# Ja otsikko kolumni
 df.drop(columns = ['Title'], inplace = True)
 df.set_index('imdbID', inplace = True)
 
 
-
+# Talletetaan edellä purettujen kolumnien sanat yhteen kolumniin
 df['bag_of_words'] = ''
 columns = df.columns
 for index, row in df.iterrows():
@@ -76,23 +81,23 @@ df.drop(columns = [col for col in df.columns if col!= 'bag_of_words'], inplace =
 
 
 
-# instantiating and generating the count matrix
+# Muodostetaan lasku matriisi
 count = CountVectorizer()
 count_matrix = count.fit_transform(df['bag_of_words'])
 
 
 
-# creating a Series for the movie titles so they are associated to an ordered numerical
-# list I will use later to match the indexes
+# creating a Series for the movie titles so they are associated to an ordered numerical, nämä kommentit säästetään tutorialista
+# list I will use later to match the indexes, nämä kommentit säästetään tutorialista
 indices = pd.Series(df.index)
 indices[:5]
 
 
 
-# generating the cosine similarity matrix
+# generating the cosine similarity matrix, nämä kommentit säästetään tutorialista
 cosine_sim = cosine_similarity(count_matrix, count_matrix)
 
-# function that takes in movie title as input and returns the top 10 recommended movies
+# function that takes in movie title as input and returns the top 10 recommended movies, nämä kommentit säästetään tutorialista
 def recommendations(title, cosine_sim = cosine_sim):
     
     recommended_movies = []
@@ -100,19 +105,19 @@ def recommendations(title, cosine_sim = cosine_sim):
     if title in indices.values:
             
 
-        # gettin the index of the movie that matches the title
+        # gettin the index of the movie that matches the imdbID, nämä kommentit säästetään tutorialista
         idx = indices[indices == title].index[0]
         
-        # creating a Series with the similarity scores in descending order
+        # creating a Series with the similarity scores in descending order, nämä kommentit säästetään tutorialista
         score_series = pd.Series(cosine_sim[idx]).sort_values(ascending = False)
 
         
         
-        # getting the indexes of the 10 most similar movies
+        # getting the indexes of the 10 most similar movies, nämä kommentit säästetään tutorialista
         top_10_indexes = list(score_series.iloc[1:11].index)
         
 
-        # populating the list with the titles of the best 10 matching movies
+        # populating the list with the titles of the best 10 matching movies, nämä kommentit säästetään tutorialista
         for i in top_10_indexes:
             recommended_movies.append({'Title': list(final_data['Title'])[i], 'imdbID': list(final_data['imdbID'])[i], 'Poster': list(final_data['Poster'])[i]})
     
